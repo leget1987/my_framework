b87cd885752680695b25064a18dce330b971948e
leget <lobanov.leget@yandex.ru>
2021-02-20 19:36:33 +0300

    hw_3

--- my_framework/templates.py
+++ my_framework/templates.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
-from jinja2 import Template
-from os import path
-from io import open
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
-    """
-    :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон
-    :param kwargs: параметры
-    :return:
-    """
-    file_path = path.join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='UTF-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # рендерим шаблон с параметрами
-    return template.render(**kwargs)
-
-
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    tmpl = env.get_template(template_name)
+    return tmpl.render(**kwargs)
